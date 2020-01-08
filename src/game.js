a1d98faaf7bb24a7f6f1eef7ff7214deefff0ea3
nesmeyanov <nesmeyanov1990@gmail.com>
2020-01-08 22:19:29 +0200

    create desk and change in desk position

--- src/game.js
+++ src/game.js
@@ -24,7 +24,48 @@ export default class Game {
 		[0,0,0,0,0,0,0,0,0,0],
 		[0,0,0,0,0,0,0,0,0,0]
 	];
-	activePiece = [
+	activePiece = {
+		x:0,
+		y:0,
+		blocks:[
+			[0,1,0],
+			[1,1,1],
+			[0,0,0]
+		]
+	};
 
-	]
+	movePieceLeft() {
+		this.activePiece.x -=1;
+
+		if (this.isPieceOutOfBounds()){
+			this.activePiece.x +=1;
+		}
+	}
+
+	movePieceRight() {
+		this.activePiece.x +=1;
+		
+		if (this.isPieceOutOfBounds()){
+			this.activePiece.x -=1;
+		}
+	}
+
+	movePieceDown() {
+		this.activePiece.y +=1;
+
+		if (this.isPieceOutOfBounds()){
+			this.activePiece.y -=1;
+		}
+	}
+
+	isPieceOutOfBounds(){
+		const playfield = this.playfield;
+		const {x,y} = this.activePiece;
+
+		return playfield[y] === undefined || playfield[x] === undefined;
+	}
+
+	lockPiece() {
+		
+	}
 }
